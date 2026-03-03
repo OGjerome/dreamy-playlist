519c902d459a15b0f75c18ae7a2d73c1bed65b27
Jérôme Oger <jerome.oger@etudiant.univ-lr.fr>
2026-03-03 14:11:09 +0100

    added database configuration for deployment env and

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from "../lib/generated/prisma/client";
+import { PrismaClient } from "./generated/prisma/PrismaClient";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_NODE !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
